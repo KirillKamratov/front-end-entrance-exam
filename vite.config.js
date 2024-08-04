98e3418c470dc5f30e5714782209dc65bdb75f89
Kirill Kamratov <kirill.kamratov@yandex.ru>
2024-08-04 03:14:50 +0300

    fixed path

--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,26 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: '.',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html'),
-      output: {
-        entryFileNames: 'js/[name].js',
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'js/[name]-[hash].js',
-      },
-    },
-  },
-  server: {
-    open: true,
-    port: 5173,
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'js'),
-    },
-  },
+	base: '/front-end-entrance-exam/',
+	root: '.',
+	build: {
+		outDir: 'dist',
+		rollupOptions: {
+			input: resolve(__dirname, 'index.html'),
+			output: {
+				entryFileNames: 'js/[name].js',
+				assetFileNames: 'assets/[name]-[hash][extname]',
+				chunkFileNames: 'js/[name]-[hash].js',
+			},
+		},
+	},
+	server: {
+		open: true,
+		port: 5173,
+	},
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, 'js'),
+		},
+	},
 });
